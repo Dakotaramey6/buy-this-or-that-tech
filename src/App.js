f42c2d81a09f5c374f6ad5d941272f8284ea5951
Dakota Ramey <Dakotaramey6@gmail.com>
2024-02-08 19:36:23 -0500

    Basic heading and tools created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [searchBarShown, setSearchBarShown] = useState(false);
+
+  const handleClick = () => {
+    setSearchBarShown((prev) => !prev);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header searchBarShown={searchBarShown} onClick={handleClick} />
     </div>
   );
 }
 
+function Header({ onClick, searchBarShown }) {
+  return (
+    <div>
+      <h1>This Or That</h1>
+      <NavBar onClick={onClick} searchBarShown={searchBarShown} />
+      <hr />
+      {searchBarShown && <SearchBar />}
+    </div>
+  );
+}
+
+function NavBar({ onClick, searchBarShown }) {
+  return (
+    <nav className="nav-bar">
+      <button>Remove Placeholder1</button>
+      <button onClick={onClick}>
+        {!searchBarShown ? "Show" : "Hide"} Search Bar
+      </button>
+      <button>Reset</button>
+      <button>Remove Placeholder2</button>
+    </nav>
+  );
+}
+
+function SearchBar() {
+  return (
+    <div className="input-bars">
+      <input id="item1" />
+      <input id="item2" />
+    </div>
+  );
+}
 export default App;
